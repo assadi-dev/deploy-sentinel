4d997cb4ec30ce06d9fae5a106c2c062079dad5f
assadi-dev <assadi.halifa@gmail.com>
2025-10-30 18:01:44 +0100

    start bot

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { ENV } from "@core/env";
 import { Client, Events, GatewayIntentBits } from "discord.js";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
 
 client.login(ENV.DISCORD_BOT_TOKEN);
