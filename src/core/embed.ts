17a32e1878b2133f7b02114a0999c01f6047b1bf
assadi-dev <assadi.halifa@gmail.com>
2025-11-01 14:46:08 +0100

    modification du type

--- src/core/embed.ts
+++ src/core/embed.ts
@@ -1,8 +1,8 @@
 import { ColorResolvable } from "discord.js";
 
-export const EMBED_COLORS: Record<string, ColorResolvable> = {
+export const EMBED_COLORS = {
   info: "#84cc16",
   success: "#16a34a",
   danger: "#dc2626",
   neutral: "##d1d5db",
-};
+} as const;
